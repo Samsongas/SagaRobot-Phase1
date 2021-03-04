52fc094a5bc113b9ef44b6fbeecd37cc31400f39
samson <samsongasparyan@gmail.com>
2021-03-04 16:29:35 +0100

    Fixed module 2 to work properly, fixed some code style

--- P1M_T03/p1m_02.hpp
+++ P1M_T03/p1m_02.hpp
@@ -11,9 +11,9 @@
 /* Prototypes */
 
 void initialize_p1m_02();
-void EnableGapsCnt(byte LeftorRight);
-void DisableGapsCnt(byte LeftorRight);
-unsigned GetGapCnt(byte LeftorRight);
-double GetSpeed(byte LeftorRight);
+void EnableGapsCnt(byte encoder_lr);
+void DisableGapsCnt(byte encoder_lr);
+unsigned GetGapCnt(byte encoder_lr);
+double GetSpeed(byte encoder_lr);
 
 #endif /* P1M_02_H */
